438a37f5e75d7c80aaeb2f8a7994216a5ad4f9fe
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-04 11:48:21 -0500

    Arithmetic operators

---
+++ operators.py
@@ -0,0 +1,29 @@
+'''
+Created on Aug 4, 2018
+
+@author: thenricksen
+'''
+age1 = 12
+age2 = 18
+ageSum = age1 + age2
+print(ageSum)
+ageDiff = age2 - age1
+print(ageDiff)
+ageMulti = age2 * age1
+print(ageMulti)
+ageDiv = age2 / age1
+print(ageDiv)
+ageMod = age2 % age1
+print(ageMod)
+
+sentence = "It was a great day I didn't have to use my AK."
+firstName = "Tom"
+lastName = "Henricksen"
+print(firstName + lastName)
+print(firstName + " " + lastName)
+
+print("Hi" * 10)
+print(sentence[0])
+print(sentence[0:8])
+print(sentence[:10])
+print(sentence[:-2])
