41fda1b156768b26d3215b495c3741b7a7a9bee9
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-05 15:05:29 -0500

    Playing with Strings

---
+++ Strings.py
@@ -0,0 +1,14 @@
+'''
+Created on Aug 5, 2018
+
+@author: thenricksen
+'''
+name = "Jake"
+name = name + " is fifteen years old"
+print(name)
+sent = "%s is fifteen years old."
+print(sent%("Ted"))
+pres = "%s %s is the President of the United States."
+print(pres%("Donald","Trump"))
+nmAge = "%s is %d years old."
+print(nmAge%("Gary",43))
