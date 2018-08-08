f81fa0260c9c4451e45ead293575d4bbfdf8474d
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-08 12:01:17 -0500

    Learning about tuples

---
+++ tuples.py
@@ -0,0 +1,16 @@
+'''
+Created on Aug 8, 2018
+
+@author: tomhenricksen
+'''
+tup = ('Oranges','Apples','Bananas')
+print(tup)
+print(tup[0])
+print(tup[1])
+print(tup[0:2])
+tup2 = (12,14)
+tup3 = tup + tup2
+print(tup3)
+del tup3
+tup4 = ("Hi")
+print(tup4 * 5)
