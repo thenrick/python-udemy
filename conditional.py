689b489ddd7c77b3a88799472fc0abb24929cd3a
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-09 13:04:29 -0500

    conditionals in python
    >,<,>=,<=,==, and, or

---
+++ conditional.py
@@ -0,0 +1,31 @@
+'''
+Created on Aug 9, 2018
+
+@author: tomhenricksen
+'''
+if(1>3): 
+    print("true")
+else:
+    print("false")
+    
+if(5>3): 
+    print("true")
+else:
+    print("false")
+
+if(5>=3): 
+    print("true")
+else:
+    print("false")
+    
+if(1>=3): 
+    print("true")
+else:
+    print("false")
+age =16
+if(age < 13):
+    print("you are young")
+elif (age >= 13 and age < 18):
+    print("you are a teenager")
+else:
+    print("you are an adult")            
