8a0d47f75efbb88b71ad3db79ccd4eb27c7e0930
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-12 16:50:00 -0500

    try and except
    Try, except, single line comments, multi-line comments

---
+++ try_except.py
@@ -0,0 +1,15 @@
+'''
+Created on Aug 12, 2018
+
+@author: tomhenricksen
+'''
+
+try:
+    if name > 3:
+        print("Hi")
+except:
+    print("Somethings wrong")
+# This is a comment
+""" 
+Multi-line comment
+"""
