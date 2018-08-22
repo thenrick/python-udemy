291128253984079bdd30a4e0aa8e806fb7e47fc7
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-22 12:58:27 -0500

    Python functions
    Learning Python functions like abs(), bool(), and etc.

---
+++ pyFunctions.py
@@ -0,0 +1,15 @@
+'''
+Created on Aug 22, 2018
+
+@author: tomhenricksen
+'''
+print(abs(-34))
+print(bool(0))
+print(bool(1))
+print(dir("string"))
+sent = "hello"
+print(help(sent.upper))
+print(eval("5*3"))
+a = 1
+print(str(a))
+print(float(a))
