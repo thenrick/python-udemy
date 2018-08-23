6669d6bbb46d5c69a5e8195994d629f46ab2f82e
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-23 14:51:56 -0400

    Objects in python
    Created a simple object file and methods

---
+++ object-oriented.py
@@ -0,0 +1,28 @@
+'''
+Created on Aug 23, 2018
+
+@author: tomhenricksen
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+        
+class Person:
+    def __init__(self, name, age):
+        '''
+        Constructor
+        '''    
+    def getName(self):
+        print(self.name)
+    def getAge(self):
+        print(self.age)  
+        
+
